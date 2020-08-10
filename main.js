20ce4eed5d6f5413cca92378fac7607c188b4f6a
Rebecca Hellström Karlsson <rebecca.hellstrom@gmail.com>
2020-08-10 14:28:30 +0200

    create object factory with mutate method

--- main.js
+++ main.js
@@ -13,10 +13,29 @@ const mockUpStrand = () => {
   return newStrand;
 };
 
+// Returns a pAequor objecy
+const pAequorFactory = (specimenNum, dna) => {
+  return {
+    specimenNum: specimenNum,
+    dna: dna,
+    mutate() {
+      const randBase = Math.floor(Math.random()*this.dna.length);
+      console.log('RandBase: ' + randBase);
+      let newBase = returnRandBase();
+      while (newBase === dna[randBase]) {
+        newBase = returnRandBase();
+      }
+      dna[randBase] = newBase;
+      console.log('newBase at index ' + randBase + ': ' + newBase);
+    },
+    compareDNA() {
+      
+    }
+  }
+}
 
-
-
-
-
-
-
+let pAequor = pAequorFactory(1, mockUpStrand());
+console.log(pAequor.dna);
+console.log('Mutating...');
+pAequor.mutate();
+console.log(pAequor.dna);
